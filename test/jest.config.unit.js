c6ef8744da58846ffc7872afb3b079b1d2acf729
AndrewGHC <drewwalsh1@gmail.com>
2018-04-16 15:50:07 +0100

    Simplify and update coverage config

--- test/jest.config.unit.js
+++ test/jest.config.unit.js
@@ -14,20 +14,12 @@ module.exports = Object.assign(
       'enzyme-to-json/serializer',
     ],
     collectCoverageFrom: [
-      // Main files
-      'app/main/events/**/*.js',
-      'app/main/utils/**/*.js',
-      'app/main/config/**/*.js',
-      'app/main/ipc/**/*.js',
-      'app/main/controllers/**/*.js',
       // Renderer files
-      'app/renderer/reducers/**/*.{js,jsx}',
-      'app/renderer/actions/**/*.{js,jsx}',
-      'app/renderer/components/**/*.{js,jsx}',
-      'app/renderer/containers/**/*.{js,jsx}',
-      'app/renderer/utils/**/*.{js,jsx}',
-      'app/renderer/ipc/**/*.{js,jsx}',
-      // Ignore index.js files
+      'app/renderer/**/*.js',
+      // Worker files
+      'app/worker/**/*.js',
+      // Ignore
+      '!**/dist/**',
       '!**/index.js',
     ],
   },
