65c6f02b6bfb38bf175671551e7aa8e96a2cc11d
AndrewGHC <drewwalsh1@gmail.com>
2018-04-16 14:14:36 +0100

    Update CheckBuiltsExists to include worker bundle

--- internals/scripts/CheckBuiltsExist.js
+++ internals/scripts/CheckBuiltsExist.js
@@ -7,6 +7,7 @@ import fs from 'fs';
 function CheckBuildsExist() {
   const mainPath = path.join(__dirname, '..', '..', 'app', 'main.prod.js');
   const rendererPath = path.join(__dirname, '..', '..', 'app', 'renderer', 'dist', 'renderer.prod.js');
+  const workerPath = path.join(__dirname, '..', '..', 'app', 'worker', 'dist', 'worker.prod.js');
 
   if (!fs.existsSync(mainPath)) {
     throw new Error(chalk.whiteBright.bgRed.bold('The main process is not built yet. Build it by running "npm run build-main"'));
@@ -14,6 +15,10 @@ function CheckBuildsExist() {
 
   if (!fs.existsSync(rendererPath)) {
     throw new Error(chalk.whiteBright.bgRed.bold('The renderer process is not built yet. Build it by running "npm run build-renderer"'));
+  }
+
+  if (!fs.existsSync(workerPath)) {
+    throw new Error(chalk.whiteBright.bgRed.bold('The worker process is not built yet. Build it by running "npm run build-renderer"'));
   }
 }
 
