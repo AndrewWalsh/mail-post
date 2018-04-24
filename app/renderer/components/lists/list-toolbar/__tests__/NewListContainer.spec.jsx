9ad6eb52998461753d1bd3dba2c90594f6f55658
AndrewGHC <drewwalsh1@gmail.com>
2018-04-24 23:50:33 +0100

    Improve NewListContainer coverage

--- app/renderer/components/lists/list-toolbar/__tests__/NewListContainer.spec.jsx
+++ app/renderer/components/lists/list-toolbar/__tests__/NewListContainer.spec.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
-import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider, Query, Mutation } from 'react-apollo';
 import configureStore from 'redux-mock-store';
 
-import { FORM_NEW_LIST } from '../../../../constants';
+import {
+  MUTATION_IMPORT_CSV,
+  QUERY_GET_LISTS,
+  FORM_NEW_LIST,
+} from '../../../../constants';
 import NewListContainer from '../NewListContainer';
 import { apolloClient } from '../../../../utils';
 
@@ -42,7 +46,31 @@ describe('NewListContainer', () => {
       listNameValue,
       nameOfList: 'newList',
     };
-
     expect(wrapper.find(NewListWrapper).props()).toEqual(expect.objectContaining(props));
+  });
+
+  it('Query hoc\'s fetch policy is "cache-and-network"', () => {
+    expect(wrapper.find(Query).prop('fetchPolicy')).toEqual('cache-and-network');
+  });
+
+  it('Query hoc\'s query is QUERY_GET_LISTS', () => {
+    expect(wrapper.find(Query).prop('query')).toEqual(QUERY_GET_LISTS);
+  });
+
+  it('Query hoc passes data to NewListWrapper', () => {
+    const queryData = wrapper.find(Query).childAt(0).prop('data');
+    const newListWrapperData = wrapper.find(NewListWrapper).prop('data');
+    expect(queryData).toEqual(newListWrapperData);
+  });
+
+  it('ReduxForm hoc passes props to NewListWrapper', () => {
+    const reduxFormProps = wrapper.find('ReduxForm').props();
+    const newListWrapperData = wrapper.find(NewListWrapper).props();
+    expect(newListWrapperData).toEqual(expect.objectContaining(reduxFormProps));
+  });
+
+  it('Mutation component mutation is MUTATION_IMPORT_CSV', () => {
+    const mutation = wrapper.find(Mutation).prop('mutation');
+    expect(mutation).toEqual(MUTATION_IMPORT_CSV);
   });
 });
