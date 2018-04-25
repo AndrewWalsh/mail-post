d005bf2514e7cd88d806861fa9fe9278bef1d1fc
AndrewGHC <drewwalsh1@gmail.com>
2018-04-25 22:37:03 +0100

    Add ListTableToolbar test

--- app/renderer/components/lists/list-table/ListTableToolbar.jsx
+++ app/renderer/components/lists/list-table/ListTableToolbar.jsx
@@ -13,16 +13,10 @@ const toolbarStyles = theme => ({
   root: {
     paddingRight: theme.spacing.unit,
   },
-  highlight:
-    theme.palette.type === 'light'
-      ? {
-        color: theme.palette.secondary.main,
-        backgroundColor: lighten(theme.palette.secondary.light, 0.85),
-      }
-      : {
-        color: theme.palette.text.primary,
-        backgroundColor: theme.palette.secondary.dark,
-      },
+  highlight: {
+    color: theme.palette.secondary.main,
+    backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+  },
   spacer: {
     flex: '1 1 100%',
   },
