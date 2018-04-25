66626131dbd4a0dac43192e04775f84e94b12b0b
AndrewGHC <drewwalsh1@gmail.com>
2018-04-25 22:57:21 +0100

    Add sort-data function & test

---
+++ app/renderer/components/lists/list-table/__tests__/sort-data.spec.js
@@ -0,0 +1,32 @@
+import td from 'testdouble';
+
+import sort from '../sort-data';
+
+require('testdouble-jest')(td, jest);
+
+describe('ListTableToolbar', () => {
+  const mockData = [{ a: 1 }, { b: 2 }, { c: 3 }];
+  const mockOrder = 'desc';
+  const mockOrderBy = 'a';
+  const mockProperty = 'a';
+
+  it('when order param is "desc" and orderBy equals property, returns { order: "asc" }', () => {
+    const { order } = sort(mockData, mockOrder, mockOrderBy, mockProperty);
+    expect(order).toBe('asc');
+  });
+
+  it('when order param is "asc" and orderBy does NOT equal property, returns { order: "desc" }', () => {
+    const { order } = sort(mockData, mockOrder, mockOrderBy, 'test');
+    expect(order).toBe('desc');
+  });
+
+  it('when order param is "desc", data is orderer in descending order', () => {
+    const { data } = sort(mockData, mockOrder, mockOrderBy, mockProperty);
+    expect(data).toEqual(mockData.reverse());
+  });
+
+  it('when order param is "asc", data is orderer in ascending order', () => {
+    const { data } = sort(mockData, 'asc', mockOrderBy, mockProperty);
+    expect(data).toEqual(mockData);
+  });
+});
---
+++ app/renderer/components/lists/list-table/sort-data.js
@@ -0,0 +1,14 @@
+export default (data, order, orderBy, property = null) => {
+  let newOrder = 'desc';
+
+  if (orderBy === property && order === 'desc') {
+    newOrder = 'asc';
+  }
+
+  const newData =
+    newOrder === 'desc'
+      ? data.sort((a, b) => (b[property] < a[property] ? -1 : 1))
+      : data.sort((a, b) => (a[property] < b[property] ? -1 : 1));
+
+  return { data: newData, order: newOrder };
+};
