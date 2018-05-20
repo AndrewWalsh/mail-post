93fc02a339a722b76493a2e7118a509af8b85396
AndrewGHC <drewwalsh1@gmail.com>
2018-05-20 15:55:45 +0100

    NewCampaignFormName component

--- app/renderer/components/campaigns/new-campaign/NewCampaignFormName.jsx
+++ app/renderer/components/campaigns/new-campaign/NewCampaignFormName.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { TextField } from 'redux-form-material-ui';
 
-import { FORM_NEW_CAMPAIGN } from '../../../constants';
+import {
+  FORM_NEW_CAMPAIGN,
+  FORM_NEW_CAMPAIGN_NAME,
+} from '../../../constants';
 
-const NewCampaignFormName = () => <Field name="username" component={TextField} placeholder="Street" />;
+const NewCampaignFormName = () => (
+  <Field
+    label="Campaign Name"
+    name={FORM_NEW_CAMPAIGN_NAME}
+    component={TextField}
+    placeholder="A name that identifies this campaign"
+    fullWidth
+  />
+);
 
 export default reduxForm({
   form: FORM_NEW_CAMPAIGN,
--- app/renderer/constants/form-names.js
+++ app/renderer/constants/form-names.js
@@ -1,2 +1,6 @@
+// Form names
 export const FORM_NEW_LIST = 'FORM_NEW_LIST';
 export const FORM_NEW_CAMPAIGN = 'FORM_NEW_CAMPAIGN';
+
+// Form field names
+export const FORM_NEW_CAMPAIGN_NAME = 'FORM_NEW_CAMPAIGN_NAME';
