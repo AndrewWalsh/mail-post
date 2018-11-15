cda59e40dee06541eb5558d2d4f21b6453a29ec8
AndrewGHC <drewwalsh1@gmail.com>
2018-11-15 12:10:06 +0000

    Cleanup stream-csv

--- app/worker/controllers/lists/import-csv/stream-csv.js
+++ app/worker/controllers/lists/import-csv/stream-csv.js
@@ -6,6 +6,7 @@ const pauseHandler = (buffer, minBufferSize, readStream) => {
 export default (readStream, writeStream, minBufferSize) => {
   // No. emails to hold in memory
   // Will be of at least length minBufer, but max length is not guaranteed
+  // Readstream watermark needs to meet max bytes of a line in the csv
   const buffer = [];
   let totalLines = 0;
   let ended = false;
@@ -24,7 +25,6 @@ export default (readStream, writeStream, minBufferSize) => {
     return buffer.splice(0, (minBufferSize - (minBufferSize - buffer.length)));
   };
 
-  // If not ended and buffer length < minBufferSize, we need to wait
   const getLines = () => new Promise(function resolver(resolve) {
     if (!ended && buffer.length < minBufferSize) return setTimeout(() => resolver(resolve), 100);
     if (ended) return resolve(splice());
@@ -36,9 +36,12 @@ export default (readStream, writeStream, minBufferSize) => {
     return resolve(splice());
   });
 
+  const getTotal = () => totalLines;
+  const hasEnded = () => ended;
+
   return {
     getLines,
-    getTotal: () => totalLines,
-    hasEnded: () => ended,
+    getTotal,
+    hasEnded,
   };
 };
